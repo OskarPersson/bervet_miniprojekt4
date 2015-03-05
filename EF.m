69a511051755981e0fdc47d8d0aac46568c7812a
MartinJohaa <martin.johansson17@gmail.com>
2015-03-05 17:04:21 +0100

    Skeleton file

---
+++ EF.m
@@ -0,0 +1,23 @@
+%%blablabla
+
+
+speed = 0;
+acc = 0;
+
+distance = 0;
+time = 0;
+steps = 0.1;
+
+while (distance < 150 && speed > 0)
+    
+%%calculate next values with (5)
+
+end
+
+while (acc < 0)
+    
+%%calculate next values with (6)
+
+end
+
+%%Maybe save distance values and time in a vect-vect and then plot!! 
